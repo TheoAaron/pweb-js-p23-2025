38e2f0f8e030a40a169b2984167cf1ce685a1daf
nightlurkr <ryanadya34@gmail.com>
2025-10-08 20:29:09 +0700

    Update types.ts

--- ts/types.ts
+++ ts/types.ts
@@ -1,16 +1,40 @@
 export interface User {
-	id: number;
-	firstName: string;
-	lastName?: string;
-	username: string;
-	email?: string;
-	[key: string]: any;
+  id: number;
+  firstName: string;
+  lastName?: string;
+  username: string;
+  email?: string;
+  [key: string]: any;
 }
 
 export interface UsersResponse {
-	users: User[];
-	total?: number;
-	skip?: number;
-	limit?: number;
+  users: User[];
+  total?: number;
+  skip?: number;
+  limit?: number;
 }
 
+export interface Recipe {
+  id: number;
+  name: string;
+  thumbnail?: string;
+  image?: string;
+  totalTime?: number;
+  time?: number;
+  difficulty?: string;
+  cuisine?: string;
+  rating?: number;
+  spoonacularScore?: number;
+  aggregateLikes?: number;
+  ingredients?: string[];
+  tags?: string[];
+  instructions?: string[] | string;
+  [key: string]: any;
+}
+
+export interface RecipesResponse {
+  recipes: Recipe[];
+  total?: number;
+  skip?: number;
+  limit?: number;
+}
